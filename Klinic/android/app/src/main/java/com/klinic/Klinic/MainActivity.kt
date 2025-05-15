2c095e57365e1eeac3fe2a5b8b54e94b08667382
Nithish Raja <nithishraja@Nithishs-MacBook-Air-2.local>
2025-05-15 16:14:17 +0530

    android build successful

--- Klinic/android/app/src/main/java/com/klinic/Klinic/MainActivity.kt
+++ Klinic/android/app/src/main/java/com/klinic/Klinic/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.Klinic
+package com.klinic.Klinic
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- Klinic/android/app/src/main/java/com/klinic/Klinic/MainApplication.kt
+++ Klinic/android/app/src/main/java/com/klinic/Klinic/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.Klinic
+package com.klinic.Klinic
 
 import android.app.Application
 import android.content.res.Configuration
